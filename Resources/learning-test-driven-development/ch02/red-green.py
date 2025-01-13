d79bfab1b7d5235a41d6494af98337edb14fac67
unknown <keep_selfs_real@naver.com>
2025-01-13 09:07:41 +0900

    feat: division and multiplication features done

---
+++ Resources/learning-test-driven-development/ch02/red-green.py
@@ -0,0 +1,36 @@
+import unittest
+
+class Dollar:
+    def __init__(self, amount):
+        self.amount = amount 
+    
+    def times(self, multiplier):
+        # return Dollar(5 * 2) ts: before abstraction
+        return Dollar(self.amount * multiplier)
+
+
+class Money:
+    def __init__(self, amount, currency):
+        self.amount = amount
+        self.currency = currency
+
+    def times(self, multiplier):
+        return Money(self.amount * multiplier, self.currency) # self.amount * multiplier 부분은 이전에 이미 이에 대한 abstraction 진행을 설명한 적 있음
+    
+
+class TestMoney(unittest.TestCase):
+    def testMultiplication(self):
+        fiver = Dollar(5)
+        tenner = fiver.times(2)
+        self.assertEqual(10, tenner.amount)
+
+        
+    def testMultiplicationInEuros(self):
+        tenEuros = Money(10, "EUR")
+        twentyEuros = tenEuros.times(2)
+        self.assertEqual(20, twentyEuros.amount)
+        self.assertEqual("EUR", twentyEuros.currency)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ Resources/learning-test-driven-development/ch02/red.py
@@ -0,0 +1,27 @@
+import unittest
+
+class Dollar:
+    def __init__(self, amount):
+        self.amount = amount 
+    
+    def times(self, multiplier):
+        # return Dollar(5 * 2) ts: before abstraction
+        return Dollar(self.amount * multiplier)
+    
+
+class TestMoney(unittest.TestCase):
+    def testMultiplication(self):
+        fiver = Dollar(5)
+        tenner = fiver.times(2)
+        self.assertEqual(10, tenner.amount)
+
+        
+    def testMultiplicationInEuros(self):
+        tenEuros = Money(10, "EUR")
+        twentyEuros = tenEuros.times(2)
+        self.assertEqual(20, twentyEuros)
+        self.assertEqual("EUR", twentyEuros.currency)
+
+
+if __name__ == "__main__":
+    unittest.main()
